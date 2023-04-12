3ff508603da75b036636001cffa4eda62eedfe6e
Mohamad Hammoud <mohamad.hammoudk@gmail.com>
2023-04-12 12:07:10 +0300

    ➕ install DLT, create invoice contract

---
+++ contracts/Invoice/Invoice.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import "dual-layer-token/contracts/DLT/DLT.sol";
+import "./interface/IInvoice.sol";
+
+contract Invoice is IInvoice, DLT {
+    constructor(
+        string memory name,
+        string memory symbol
+    ) DLT("Polytrade Invoice Collection", "PIC") {}
+}
