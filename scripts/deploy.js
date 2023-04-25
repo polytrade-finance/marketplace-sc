09dad031fc638081993cdb49c340fa02e8736ba8
Mohamad Hammoud <mohamad.hammoudk@gmail.com>
2023-04-25 13:23:51 +0300

    🔨 added the verify script to the deployment script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -18,7 +18,6 @@ async function main() {
 
   const FormulasFactory = await hre.ethers.getContractFactory("Formulas");
   const formulasContract = await FormulasFactory.deploy();
-
   await formulasContract.deployed();
   console.log("formulasContract deployed to:", formulasContract.address);
 
@@ -49,12 +48,37 @@ async function main() {
   );
   await marketplaceContract.deployed();
   console.log("marketplaceContract deployed to:", marketplaceContract.address);
+
+  await hre.run("verify:verify", {
+    address: formulasContract.address,
+    constructorArguments: [],
+  });
+
+  await hre.run("verify:verify", {
+    address: formulasContract.address,
+    constructorArguments: [
+      "Polytrade Invoice Collection",
+      INVOICE_COLLECTION_SYMBOL,
+      INVOICE_COLLECTION_BASE_URI,
+      formulasContract.address,
+    ],
+  });
+
+  await hre.run("verify:verify", {
+    address: formulasContract.address,
+    constructorArguments: [TOKEN_NAME, TOKEN_SYMBOL, BUYER_PUBLIC_KEY, 200000],
+  });
+
+  await hre.run("verify:verify", {
+    address: formulasContract.address,
+    constructorArguments: [invoiceContract.address, stableCoinContract.address],
+  });
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().then(() => {
-  throw new Error("error");
+  throw new Error("Succeeded");
 });
 
 // npx hardhat run --network mumbai scripts/deploy.js
