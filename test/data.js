6361956c2147018785d7028eed079037ecf1c62e
Mohamad Hammoud <mohamad.hammoudk@gmail.com>
2023-04-26 11:23:24 +0300

    🔨 added parseUnits functionality

--- test/data.js
+++ test/data.js
@@ -1,23 +1,30 @@
 const { ethers } = require("hardhat");
 
+const DECIMALS = {
+  TWO: 2,
+  SIX: 6,
+  EIGHTEEN: 18,
+};
+
 const invoice1 = {
   initialMainMetadata: {
-    lateFeePercentage: 10,
-    gracePeriod: 10,
-    dueDate: Number(new Date("2022-11-12").getTime() / 1000),
-    invoiceDate: Number(new Date("2022-10-10").getTime() / 1000),
-    fundsAdvancedDate: Number(new Date("2022-10-13").getTime() / 1000),
-    invoiceAmount: ethers.utils.parseEther("10000"),
+    lateFeePercentage: ethers.utils.parseUnits("10", DECIMALS.TWO),
+    gracePeriod: 10, // in days
+    dueDate: Number(new Date("2022-11-12").getTime() / 1000), // in seconds
+    invoiceDate: Number(new Date("2022-10-10").getTime() / 1000), // in seconds
+    fundsAdvancedDate: Number(new Date("2022-10-13").getTime() / 1000), // in seconds
+    invoiceAmount: ethers.utils.parseUnits("10000", DECIMALS.SIX),
   },
   initialSubMetadata: {
-    factoringFeePercentage: 10,
-    discountFeePercentage: 10,
-    bankChargesFeeAmount: 10,
-    additionalFeeAmount: 10,
-    advanceFeePercentage: 1000, // %10
+    factoringFeePercentage: ethers.utils.parseUnits("10", DECIMALS.TWO),
+    discountFeePercentage: ethers.utils.parseUnits("10", DECIMALS.TWO),
+    bankChargesFeeAmount: ethers.utils.parseUnits("10", DECIMALS.SIX),
+    additionalFeeAmount: ethers.utils.parseUnits("10", DECIMALS.SIX),
+    advanceFeePercentage: ethers.utils.parseUnits("90", DECIMALS.TWO), // %90
   },
 };
 
 module.exports = {
   invoice1,
+  DECIMALS,
 };
