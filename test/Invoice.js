c38a4efc2f77dca76a17997a4108ba73e2ce9968
zakrad <1@1.com>
2023-05-30 11:14:49 +0330

    ✅ add coverage tests

--- test/Invoice.js
+++ test/Invoice.js
@@ -1,13 +1,18 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
-const { invoice } = require("./data");
+const { invoice, YEAR } = require("./data");
+const { now } = require("./helpers");
 
 describe("Invoice", function () {
   let invoiceContract;
   let deployer;
   let user1;
+  let currentTime;
+
   beforeEach(async () => {
     [deployer, user1] = await ethers.getSigners();
+
+    currentTime = await now();
 
     const InvoiceFactory = await ethers.getContractFactory("Invoice");
     invoiceContract = await InvoiceFactory.deploy(
@@ -19,7 +24,7 @@ describe("Invoice", function () {
     await invoiceContract.deployed();
   });
 
-  it("Create Invoice successfully", async function () {
+  it("Should create invoice successfully", async function () {
     expect(
       await invoiceContract.createInvoice(
         deployer.address,
@@ -37,7 +42,7 @@ describe("Invoice", function () {
     expect(await invoiceContract.tokenURI(1)).to.eq(`https://ipfs.io/ipfs${1}`);
   });
 
-  it("Revert on Creating minted invoice", async function () {
+  it("Should revert on creating minted invoice", async function () {
     expect(
       await invoiceContract.createInvoice(
         deployer.address,
@@ -61,7 +66,7 @@ describe("Invoice", function () {
     ).to.revertedWith("Invoice: Already minted");
   });
 
-  it("Revert on Creating invoice by invalid caller", async function () {
+  it("Should revert on creating invoice by invalid caller", async function () {
     await expect(
       invoiceContract
         .connect(user1)
@@ -75,12 +80,12 @@ describe("Invoice", function () {
     ).to.be.reverted;
   });
 
-  it("Set new base uri", async function () {
+  it("Should to set new base uri", async function () {
     await expect(invoiceContract.setBaseURI("https://ipfs2.io/ipfs")).to.not.be
       .reverted;
   });
 
-  it("Revert Set new base uri by invalid caller", async function () {
+  it("Should revert to set new base uri by invalid caller", async function () {
     await expect(
       invoiceContract.connect(user1).setBaseURI("https://ipfs2.io/ipfs")
     ).to.be.reverted;
@@ -101,7 +106,7 @@ describe("Invoice", function () {
 
     expect(await invoiceContract.mainBalanceOf(user1.address, 3)).to.eq(1);
   });
-  it("Revert Batch create invoices on wrong array parity", async function () {
+  it("Should revert Batch create invoices on wrong array parity", async function () {
     await expect(
       invoiceContract.batchCreateInvoice(
         [user1.address, user1.address, user1.address],
@@ -131,7 +136,7 @@ describe("Invoice", function () {
     ).to.be.revertedWith("Invoice: No array parity");
   });
 
-  it("Revert on Batch Creating invoices by invalid caller", async function () {
+  it("Should revert on batch creating invoices by invalid caller", async function () {
     await expect(
       invoiceContract
         .connect(user1)
@@ -143,5 +148,52 @@ describe("Invoice", function () {
           [invoice.dueDate, invoice.dueDate, invoice.dueDate]
         )
     ).to.be.reverted;
+  });
+  it("Should create an invoice and get remaining rewards", async function () {
+    expect(
+      await invoiceContract.createInvoice(
+        deployer.address,
+        1,
+        invoice.assetPrice,
+        invoice.rewardApr,
+        invoice.dueDate
+      )
+    )
+      .to.emit(invoiceContract, "InvoiceCreated")
+      .withArgs(deployer.address, deployer.address, 1);
+
+    const tenure = invoice.dueDate - currentTime;
+    const reward =
+      ((invoice.rewardApr / 10000) * tenure * invoice.assetPrice) / YEAR;
+    const expectedReward = Math.round(reward);
+    const actualReward = await invoiceContract.getRemainingReward(1);
+
+    expect(actualReward).to.be.within(expectedReward - 1, expectedReward + 1);
+  });
+
+  it("Should return zero rewards for minted invoice with zero price", async function () {
+    expect(
+      await invoiceContract.createInvoice(
+        deployer.address,
+        1,
+        0,
+        invoice.rewardApr,
+        invoice.dueDate
+      )
+    )
+      .to.emit(invoiceContract, "InvoiceCreated")
+      .withArgs(deployer.address, deployer.address, 1);
+
+    const expectedReward = 0;
+    const actualReward = await invoiceContract.getRemainingReward(1);
+
+    expect(actualReward).to.be.equal(expectedReward);
+  });
+
+  it("Should return zero rewards for not minted invoice", async function () {
+    const expectedReward = 0;
+    const actualReward = await invoiceContract.getRemainingReward(2);
+
+    expect(actualReward).to.be.equal(expectedReward);
   });
 });
