99854bf6394e1945071950aef0237f6cdeba3f67
zakrad <daniyal.rad@gmail.com>
2023-09-19 12:49:55 +0330

    ✅ modify data

--- test/data.js
+++ test/data.js
@@ -1,4 +1,5 @@
 const { ethers } = require("hardhat");
+const { now } = require("./helpers");
 
 const DECIMALS = {
   TWO: 2,
@@ -10,20 +11,59 @@ const MarketplaceAccess = ethers.keccak256(
   ethers.toUtf8Bytes("MARKETPLACE_ROLE")
 );
 
+const OriginatorAccess = ethers.keccak256(
+  ethers.toUtf8Bytes("ASSET_ORIGINATOR")
+);
+
+const AssetManagerAccess = ethers.keccak256(
+  ethers.toUtf8Bytes("ASSET_MANAGER")
+);
+
 const DAY = 24n * 60n * 60n;
 const YEAR = 360n * DAY;
 
 const asset = {
-  assetPrice: ethers.parseUnits("10", DECIMALS.SIX),
-  rewardApr: ethers.parseUnits("10", DECIMALS.TWO), // with 2 decimals
+  price: ethers.parseUnits("10", DECIMALS.SIX),
   dueDate: Number(new Date("2023-11-12").getTime() / 1000), // in seconds
-  minFraction: 1000, // from 10000
-  lastSale: 0,
-  lastClaim: 0,
+  rewardApr: ethers.parseUnits("10", DECIMALS.TWO), // with 2 decimals
+  fractions: 10000,
+};
+
+const zeroPriceAsset = {
+  price: 0,
+  dueDate: Number(new Date("2023-11-12").getTime() / 1000), // in seconds
+  rewardApr: ethers.parseUnits("10", DECIMALS.TWO), // with 2 decimals
+  fractions: 10000,
+};
+
+const nearSettleAsset = async () => {
+  return {
+    price: ethers.parseUnits("10", DECIMALS.SIX),
+    dueDate: (await now()) + 100, // in seconds
+    rewardApr: ethers.parseUnits("10", DECIMALS.TWO), // with 2 decimals
+    fractions: 10000,
+  };
+};
+
+const nearSettleProperty = async () => {
+  return {
+    price: ethers.parseUnits("10", DECIMALS.SIX),
+    dueDate: (await now()) + 100,
+    fractions: 10000,
+    size: 200,
+    bathrooms: 2,
+    rooms: 3,
+    constructionDate: Number(new Date("2020-11-12").getTime() / 1000),
+    country: "Italy",
+    city: "Rome",
+    location: "https://goo.gl/maps/bEBPrewmyJB8v6CG8",
+  };
 };
 
 const property = {
-  value: 3000,
+  price: ethers.parseUnits("10", DECIMALS.SIX),
+  dueDate: Number(new Date("2023-11-12").getTime() / 1000),
+  fractions: 10000,
   size: 200,
   bathrooms: 2,
   rooms: 3,
@@ -42,8 +82,13 @@ module.exports = {
   DAY,
   YEAR,
   asset,
+  zeroPriceAsset,
+  nearSettleAsset,
+  nearSettleProperty,
   property,
   offer,
   DECIMALS,
   MarketplaceAccess,
+  OriginatorAccess,
+  AssetManagerAccess,
 };
