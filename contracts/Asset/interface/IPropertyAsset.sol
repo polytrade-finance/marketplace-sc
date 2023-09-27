488798633c0fc553ea638b773cc5b8fa56130c00
zakrad <daniyal.rad@gmail.com>
2023-09-27 18:30:20 +0330

    add treasury wallet to property

--- contracts/Asset/interface/IPropertyAsset.sol
+++ contracts/Asset/interface/IPropertyAsset.sol
@@ -5,6 +5,16 @@ import { PropertyInfo } from "contracts/lib/structs.sol";
 
 interface IPropertyAsset {
     /**
+     * @dev Emitted when new `Treasury Wallet` has been set
+     * @param oldTreasuryWallet, Address of the old treasury wallet
+     * @param newTreasuryWallet, Address of the new treasury wallet
+     */
+    event TreasuryWalletSet(
+        address oldTreasuryWallet,
+        address newTreasuryWallet
+    );
+
+    /**
      * @dev Emitted when an asset is settled
      * @param owner, address of the asset owner
      * @param propertyMainId, propertyMainId identifier
@@ -22,6 +32,12 @@ interface IPropertyAsset {
      * @dev Reverted on unsupported interface detection
      */
     error UnsupportedInterface();
+
+    /**
+     * @dev Allows to set a new treasury wallet address where funds will be allocated.
+     * @param newTreasuryWallet, Address of the new treasury wallet
+     */
+    function setTreasuryWallet(address newTreasuryWallet) external;
 
     /**
      * @dev Creates an property with its parameters
@@ -71,6 +87,12 @@ interface IPropertyAsset {
         uint256 propertySubId,
         uint256 amount
     ) external;
+
+    /**
+     * @dev Gets current treasury wallet address
+     * @return address, Address of the treasury wallet
+     */
+    function getTreasuryWallet() external view returns (address);
 
     /**
      * @dev Gets the property information
