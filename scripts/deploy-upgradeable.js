84dcf6c63707b61e5a3be749a3790a1604553fc2
zakrad <daniyal.rad@gmail.com>
2023-12-11 11:09:00 +0330

    🚨 fix lint warnings

--- scripts/deploy-upgradeable.js
+++ scripts/deploy-upgradeable.js
@@ -68,15 +68,11 @@ async function main() {
 
   await asset.grantRole(AssetManagerAccess, invoiceAsset.getAddress());
 
-
   await asset.grantRole(AssetManagerAccess, propertyAsset.getAddress());
-
 
   await asset.grantRole(AssetManagerAccess, wrapperAsset.getAddress());
 
-
   await invoiceAsset.grantRole(MarketplaceAccess, marketplace.getAddress());
-
 
   await asset.setApprovalForAll(marketplace.getAddress(), true);
 }
